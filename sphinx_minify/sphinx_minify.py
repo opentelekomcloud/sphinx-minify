1ebcdaae8d8e7ce33f18f19129716cd28de9c449
Sebastian Gode <sebastian.gode@telekom.de>
2023-10-05 13:25:43 +0000

    Working single threading

--- sphinx_minify/sphinx_minify.py
+++ sphinx_minify/sphinx_minify.py
@@ -5,7 +5,7 @@ from htmlmin import minify as htmlmin
 import logging
 
 # Function to minify JavaScript and CSS based on file extension
-def minify_file(input_file, output_file):
+def minify_file(input_file):
     try:
         with open(input_file, 'r') as file:
             file_contents = file.read()
@@ -27,26 +27,30 @@ def minify_file(input_file, output_file):
         logging.warning(f"Unable to minify {input_file}")
         return
 
-    with open(output_file, 'w') as file:
-        file.write(minified_contents)
+    return minified_contents
 
 # Function to recursively traverse a directory and minify files
-def minify_directory(directory):
-    for root, _, files in os.walk(directory):
+def minify_directory(input_directory, output_directory):
+    for root, _, files in os.walk(input_directory):
         for singlefile in files:
             if ".min" in singlefile:
                 continue
-            print(singlefile)
             input_file = os.path.join(root, singlefile)
-            output_file = (
-                os.path.splitext(input_file)[0]
-                + ".min"
-                + os.path.splitext(input_file)[1]
-            )
-            minify_file(input_file, output_file)
+            relative_path = os.path.relpath(input_file, input_directory)
+            output_file = os.path.join(output_directory, relative_path)
+            
+            # Ensure the output directory structure exists
+            os.makedirs(os.path.dirname(output_file), exist_ok=True)
+            
+            output = minify_file(input_file)
+            if output:
+                with open(output_file, 'w') as file:
+                    file.write(output)
 
 def main():
-    directory_to_minify = "doc/build/html_copy"  # Replace with the target directory path
-    minify_directory(directory_to_minify)
+    input_directory = "doc/build/html_copy"
+    output_directory = "doc/build/html_copy/out"
+    logging.info("Starting to minify all output files...")
+    minify_directory(input_directory, output_directory)
 
 main()
